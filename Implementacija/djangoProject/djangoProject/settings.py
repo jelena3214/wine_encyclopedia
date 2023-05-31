4dbd33a162b3b61e7d37673f526403d0cc9520d2
cj200305d <cj200305d@student.etf.bg.ac.rs>
2023-06-01 01:10:14 +0200

    Sve slike ce biti u istom folderu
    Change-Id: Ib3a023e17c72cfb367593836251a7bf4ad2c54b3

--- Implementacija/djangoProject/djangoProject/settings.py
+++ Implementacija/djangoProject/djangoProject/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
