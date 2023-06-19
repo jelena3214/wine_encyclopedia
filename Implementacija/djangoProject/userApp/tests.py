e66dfaa677ccfbfa09e8b6de957dbd84fcb7d6ef
mb200235d <mb200235d@student.etf.bg.ac.rs>
2023-06-19 02:21:38 +0200

    Dodat prvi deo Django testova za userApp aplikaciju
    Change-Id: Ic1048af2c53b2e6b4829c6232e231b3dd552693d

--- Implementacija/djangoProject/userApp/tests.py
+++ Implementacija/djangoProject/userApp/tests.py
@@ -1,3 +1,164 @@
+from datetime import datetime
+import os
+from django.contrib.auth.models import Group
+from django.urls import reverse
+from baza.models import *
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+"""
+    Author: Bojana Malesevic 2020/0235
+"""
 
-# Create your tests here.
+
+class UserRegistrationTestCase(TestCase):
+
+    existingBuyer = None
+    existingProducer = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.group = Group(name='Kupci')
+        cls.group.save()
+        cls.group = Group(name='Proizvodjaci')
+        cls.group.save()
+        cls.existingBuyer = Kupac.objects.create_user(
+            email='existinguser@example.com',
+            password='testpassword',
+            first_name='Jane',
+            last_name='Smith',
+            brtelefona='987654321',
+            adresa='Ulica 1',
+            datumrodjenja=datetime(year=1995, month=7, day=17),
+        )
+        cls.existingBuyer.groups.add(Group.objects.get(name='Kupci'))
+        cls.existingBuyer.save()
+
+        cls.existingProducer = Proizvodjac.objects.create_user(
+            email='existingproducer@example.com',
+            password='testpassword',
+            imefirme='Existing Winery',
+            registarskibroj='54321',
+            brtelefona='987654321',
+            adresa='456 Vineyard St',
+            opis='Existing winery description',
+            logo=SimpleUploadedFile('images/1234.jpg', open('static/images/1234.jpg', 'rb').read())
+        )
+        cls.existingProducer.groups.add(Group.objects.get(name='Proizvodjaci'))
+        cls.existingProducer.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Clean up code after all tests
+        if os.path.exists(cls.existingProducer.logo.path):
+            os.remove(cls.existingProducer.logo.path)
+
+    def test_register_user(self):
+        response = self.client.post(reverse('registerUser'), {
+            'email': 'testuser@example.com',
+            'password': 'testpassword',
+            'firstName': 'John',
+            'lastName': 'Doe',
+            'phoneNumber': '123456789',
+            'address': 'Street 123',
+            'birthDate': '1990-01-01',
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('loginUser'))
+
+        # Check if the user is created in the database
+        user = Korisnik.objects.get(email='testuser@example.com')
+        self.assertEqual(user.first_name, 'John')
+        self.assertEqual(user.last_name, 'Doe')
+        self.assertEqual(user.kupac.brtelefona, '123456789')
+        self.assertEqual(user.kupac.adresa, 'Street 123')
+
+        # Check if the user is assigned to the 'Kupci' group
+        kupciGroup = Group.objects.get(name='Kupci')
+        self.assertIn(kupciGroup, user.groups.all())
+
+    def test_register_existing_user(self):
+        response = self.client.post(reverse('registerUser'), {
+            'email': 'existinguser@example.com',
+            'password': 'testpassword',
+            'firstName': 'Jane',
+            'lastName': 'Smith',
+            'phoneNumber': '987654321',
+            'address': '456 Elm St',
+            'birthDate': '1995-01-01',
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registracijaKupac.html')
+        self.assertContains(response, 'Već postoji nalog sa unetom email adresom')
+
+    def test_register_producer(self):
+        response = self.client.post(reverse('registerProducer'), {
+            'email': 'testproducer@example.com',
+            'password': 'testpassword',
+            'name': 'Test Winery',
+            'companyNumber': '12345',
+            'phoneNumber': '123456789',
+            'address': '123 Vineyard St',
+            'description': 'Test winery description',
+            'logo': SimpleUploadedFile('images/1234.jpg', open('static/images/1234.jpg', 'rb').read())
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('loginUser'))
+
+        # Check if the producer is created in the database
+        producer = Proizvodjac.objects.get(email='testproducer@example.com')
+        self.assertEqual(producer.imefirme, 'Test Winery')
+        self.assertEqual(producer.javnoime, 'Test Winery')
+        self.assertEqual(producer.registarskibroj, 12345)
+        self.assertEqual(producer.brtelefona, '123456789')
+        self.assertEqual(producer.adresa, '123 Vineyard St')
+        self.assertEqual(producer.opis, 'Test winery description')
+        self.assertRegex(producer.logo.name, r"^images/1234_[a-zA-Z0-9]{7}\.jpg$")
+
+        # Check if the producer is assigned to the 'Proizvodjaci' group
+        proizvodjaciGroup = Group.objects.get(name='Proizvodjaci')
+        self.assertIn(proizvodjaciGroup, producer.groups.all())
+
+        if os.path.exists(producer.logo.path):
+            os.remove(producer.logo.path)
+
+    def test_register_existing_producer(self):
+        response = self.client.post(reverse('registerProducer'), {
+            'email': 'existingproducer@example.com',
+            'password': 'testpassword',
+            'name': 'Existing Winery',
+            'companyNumber': '54321',
+            'phoneNumber': '987654321',
+            'address': '456 Vineyard St',
+            'description': 'Existing winery description',
+            'logo': SimpleUploadedFile('images/1234.jpg', open('static/images/1234.jpg', 'rb').read())
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registracijaProizvodjac.html')
+        self.assertContains(response, 'Već postoji nalog sa unetom email adresom')
+
+    def test_login_user(self):
+        response = self.client.post(reverse('loginUser'), {
+            'email': 'existinguser@example.com',
+            'password': 'testpassword',
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
+
+        # Check if the user is authenticated
+        user = Korisnik.objects.get(email='existinguser@example.com')
+        self.assertTrue(user.is_authenticated)
+
+    def test_login_invalid_credentials(self):
+        response = self.client.post(reverse('loginUser'), {
+            'email': 'existinguser@example.com',
+            'password': 'wrongpassword',
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+        self.assertContains(response, 'Pogresna lozinka ili email adresa')
--- Implementacija/djangoProject/views/tests.py
+++ Implementacija/djangoProject/views/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from baza.models import *
-# from project_FourDesperados.Implementacija.djangoProject.baza.models import *
+
+"""
+    Author: Aleksa Boricic 2020/0294
+"""
+
 
 # Helper Functions
-def createUser(username,password):
-
+def createUser(username, password):
     user = Proizvodjac()
     user.javnoime = "Naidobrata Firma"
     user.registarskibroj = 7777
@@ -14,65 +17,75 @@ def createUser(username,password):
     user.save()
     return user
 
+
 def createOffer(user):
     offer = Ponuda()
     offer.idkorisnik = user
     offer.save()
     return offer
 
+
 def createOfferSpace(offer):
     offerspace = Ponudaprostor(idponuda=offer)
     offerspace.save()
     return offerspace
-def createWine(naziv,cena,opisVina,offer : Ponuda):
-    wine = Vino(naziv=naziv,cena=cena,idponuda=offer)
+
+
+def createWine(naziv, cena, opisVina, offer: Ponuda):
+    wine = Vino(naziv=naziv, cena=cena, idponuda=offer)
     wine.save()
     return wine
+
 
 def createPicture(offer):
     slika = Slika(idponuda=offer)
     slika.save()
     return slika
 
-def createTag(text,wine):
-    tag = Tag(tag=text,idponuda=wine)
+
+def createTag(text, wine):
+    tag = Tag(tag=text, idponuda=wine)
     tag.save()
     return tag
 
-def createDetour(offerspace,sommelier_price):
-    detour = Obilazak(idponuda=offerspace,cenasomelijera=sommelier_price)
+
+def createDetour(offerspace, sommelier_price):
+    detour = Obilazak(idponuda=offerspace, cenasomelijera=sommelier_price)
     detour.save()
     return detour
 
-def createDetourType(name,price,description,detour):
-    detourType = Vrstaobilaska(naziv=name,cena=price,opis=description,idponuda=detour)
+
+def createDetourType(name, price, description, detour):
+    detourType = Vrstaobilaska(naziv=name, cena=price, opis=description, idponuda=detour)
     detourType.save()
     return detourType
 
-def createCelebration(offerspace,description,price,capacity):
-    celebration = Proslava(idponuda=offerspace,opisproslave=description,cenapoosobi=price,kapacitet=capacity)
+
+def createCelebration(offerspace, description, price, capacity):
+    celebration = Proslava(idponuda=offerspace, opisproslave=description, cenapoosobi=price, kapacitet=capacity)
     celebration.save()
     return celebration
+
 
 class ViewTest(TestCase):
 
     def test_allwines(self):
-        user = createUser("TestKorisnik","T3STS1FR@MIRLJUBAVSRECADECACVECA")
+        user = createUser("TestKorisnik", "T3STS1FR@MIRLJUBAVSRECADECACVECA")
         offer = createOffer(user)
-        wine = createWine("Test Vino",777,"Pravimo cistaka za zdrave plucne maramice",offer)
+        wine = createWine("Test Vino", 777, "Pravimo cistaka za zdrave plucne maramice", offer)
         slika = createPicture(offer)
-        tag = createTag("Belo",wine)
+        tag = createTag("Belo", wine)
         response = self.client.get("/views/allwines")
-        self.assertContains(response,"Test Vino",html=True)
+        self.assertContains(response, "Test Vino", html=True)
 
         # Tests if the wine appears at all
-        response = self.client.post("/views/allwines",{'winery' : user.javnoime})
-        self.assertContains(response,"Test Vino",html=True)
+        response = self.client.post("/views/allwines", {'winery': user.javnoime})
+        self.assertContains(response, "Test Vino", html=True)
 
         # Tests if the wine appears with all the possible filters and sorts enabled
         response = self.client.post("/views/allwines", {'winery': user.javnoime,
-                                                        'sort' : 'Po ceni opadajuce',
-                                                        'filter' : tag.tag})
+                                                        'sort': 'Po ceni opadajuce',
+                                                        'filter': tag.tag})
         self.assertContains(response, "Test Vino", html=True)
 
     def test_wine_specific(self):
@@ -83,27 +96,25 @@ class ViewTest(TestCase):
         id = "0" + str(offer.idponuda)
 
         response = self.client.get("/views/wine/" + id)
-        self.assertContains(response,"Test vino", html=True)
-
+        self.assertContains(response, "Test vino", html=True)
 
     def test_detours(self):
         user = createUser("TestKorisnik", "T3STS1FR@MIRLJUBAVSRECADECACVECA")
         offer = createOffer(user)
         offerspace = createOfferSpace(offer)
-        detour = createDetour(offerspace,1800)
+        detour = createDetour(offerspace, 1800)
         slika = createPicture(offer)
-        detourType = createDetourType("Profi Obilazak",1700,"Obilazak je veoma profi hladni smo ko led zestoki ko dzek",detour)
+        detourType = createDetourType("Profi Obilazak", 1700, "Obilazak je veoma profi", detour)
 
-        #Tests if the detour appears when all are displayed
+        # Tests if the detour appears when all are displayed
         response = self.client.get("/views/detour")
-        self.assertContains(response,user.javnoime,html=True)
-
-        #Tests if the detour appears/doesn't when a filter is applied
-        response = self.client.post("/views/detour",{'filter' : 'Ima somelijera'})
-        self.assertNotContains(response,user.javnoime,html=True)
-        response = self.client.post("/views/detour",{'filter' : 'Nema somelijera'})
         self.assertContains(response, user.javnoime, html=True)
 
+        # Tests if the detour appears/doesn't when a filter is applied
+        response = self.client.post("/views/detour", {'filter': 'Ima somelijera'})
+        self.assertNotContains(response, user.javnoime, html=True)
+        response = self.client.post("/views/detour", {'filter': 'Nema somelijera'})
+        self.assertContains(response, user.javnoime, html=True)
 
     def test_detour_specific(self):
         user = createUser("TestKorisnik", "T3STS1FR@MIRLJUBAVSRECADECACVECA")
@@ -112,17 +123,17 @@ class ViewTest(TestCase):
         detour = createDetour(offerspace, 1800)
         slika = createPicture(offer)
         detourtype = createDetourType("Profi Obilazak", 1700,
-                                      "Obilazak je veoma profi hladni smo ko led zestoki ko dzek", detour)
+                                      "Obilazak je veoma profi", detour)
         detour_id = "0" + user.javnoime
         response = self.client.get("/views/detours/" + detour_id)
-        self.assertContains(response,user.javnoime,html=True)
-        self.assertContains(response,detourtype.naziv,html=True)
+        self.assertContains(response, user.javnoime, html=True)
+        self.assertContains(response, detourtype.naziv, html=True)
 
     def test_celebrations(self):
         user = createUser("TestKorisnik", "T3STS1FR@MIRLJUBAVSRECADECACVECA")
         offer = createOffer(user)
         offerspace = createOfferSpace(offer)
-        proslava = createCelebration(offerspace, "opis mnogo super proslave Tasha volimo te", 1700, 100)
+        proslava = createCelebration(offerspace, "opis mnogo super proslave", 1700, 100)
         slika = createPicture(offer)
         response = self.client.get("/views/celebration")
         self.assertContains(response, user.javnoime, html=True)
@@ -131,26 +142,11 @@ class ViewTest(TestCase):
         user = createUser("TestKorisnik", "T3STS1FR@MIRLJUBAVSRECADECACVECA")
         offer = createOffer(user)
         offerspace = createOfferSpace(offer)
-        proslava=  createCelebration(offerspace,"opis mnogo super proslave Tasha volimo te",1700,100)
+        proslava = createCelebration(offerspace, "opis mnogo super proslave", 1700, 100)
         slika = createPicture(offer)
         celebration_id = "0" + user.javnoime
         response = self.client.get("/views/celebration/" + celebration_id)
-        #Tests if everything appears correctly
-        self.assertContains(response,"opis mnogo super proslave Tasha volimo te",html=True)
-        self.assertContains(response,"Kapacitet sale: 100 osoba",html=True)
-        self.assertContains(response,"Cena po osobi: 1700 RSD",html=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # Tests if everything appears correctly
+        self.assertContains(response, "opis mnogo super proslave", html=True)
+        self.assertContains(response, "Kapacitet sale: 100 osoba", html=True)
+        self.assertContains(response, "Cena po osobi: 1700 RSD", html=True)
